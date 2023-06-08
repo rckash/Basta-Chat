8de87d0074fffb3fb6aaa58629e755e6c7dfee2d
RC <rckashyapcpe@gmail.com>
2023-06-08 22:20:54 +0800

    working on uploading image and converting it into a bitmap

--- app/src/main/java/com/example/bastachat/HomeActivity.kt
+++ app/src/main/java/com/example/bastachat/HomeActivity.kt
@@ -1,23 +1,51 @@
 package com.example.bastachat
 
+import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.graphics.createBitmap
 import com.example.bastachat.databinding.ActivityHomeBinding
-import com.example.bastachat.databinding.ActivityLoginBinding
+import com.google.android.play.integrity.internal.c
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import java.io.InputStream
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
     private lateinit var fAuth: FirebaseAuth
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if(it.resultCode == RESULT_OK) {
+            Log.d("HomeActivity","Activity result: ${it.data}")
+            contentResolver
+
+            val inputStream = contentResolver.openInputStream(intent.data!!)
+            val uri = intent.data
+            Log.d("HomeActivity","URI is: $uri")
+            val myDrawable = Drawable.createFromStream(inputStream, uri.toString())
+            binding.btnUploadPhoto.background = uri
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Initialize viewBinding
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         // Initialize Firebase Auth
         fAuth = Firebase.auth
 
@@ -28,19 +56,16 @@ class HomeActivity : AppCompatActivity() {
             logOut()
             exitIfNotLoggedIn()
         }
-        binding.btnUploadPhoto.setOnClickListener {  }
+        binding.btnUploadPhoto.setOnClickListener {
+            Log.d("HomeActivity","try to show photo selector")
+
+            // Launch the photo picker and let the user choose only images.
+            openImageSelector()
+        }
     }
     override fun onStart() {
         super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
         exitIfNotLoggedIn()
-    }
-    private fun uploadPhoto() {
-        Log.d("HomeActivity","try to show photo selector")
-
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/"
-        startActivity(intent)
     }
     private fun exitIfNotLoggedIn() {
         val currentUser = fAuth.currentUser
@@ -53,5 +78,15 @@ class HomeActivity : AppCompatActivity() {
     private fun logOut() {
         FirebaseAuth.getInstance().signOut()
         Log.d("HomeActivity","Logging out")
+    }
+    private fun openImageSelector() {
+        val myImageSelectorIntent = Intent(Intent.ACTION_PICK)
+        myImageSelectorIntent.data = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+//        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        launcher.launch(myImageSelectorIntent)
+
+
+
     }
 }
--- app/src/main/java/com/example/bastachat/MainActivity.kt
+++ app/src/main/java/com/example/bastachat/MainActivity.kt
@@ -4,13 +4,20 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.bastachat.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var fAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //viewBinding initialization
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        //Firebase initialization
+        fAuth = Firebase.auth
 
         //on-click functionalities
         binding.txtAlreadyHaveAnAccount.setOnClickListener { goToLoginActivity() }
